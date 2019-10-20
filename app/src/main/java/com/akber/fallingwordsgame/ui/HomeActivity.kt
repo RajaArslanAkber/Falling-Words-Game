996a5dc8eaaf2e6d322e10f93838002c2a5d4b2f
arslan.akber <arslan.akber@targetofs.com>
2019-10-20 23:51:35 +0500

    landing ui defined

--- app/src/main/java/com/akber/fallingwordsgame/ui/HomeActivity.kt
+++ app/src/main/java/com/akber/fallingwordsgame/ui/HomeActivity.kt
@@ -3,10 +3,19 @@ package com.akber.fallingwordsgame.ui
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.akber.fallingwordsgame.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
-
+        when (v?.id) {// user has choice which language he need to learn
+            R.id.eng_btn -> {
+                startGame(true)
+            }
+            R.id.spa_btn -> {
+                startGame(false)
+            }
+        }
     }
 
     private fun startGame(toLearnSpanish: Boolean) {
@@ -15,6 +24,9 @@ class HomeActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
 
+        spa_btn.setOnClickListener(this)
+        eng_btn.setOnClickListener(this)
     }
 }
