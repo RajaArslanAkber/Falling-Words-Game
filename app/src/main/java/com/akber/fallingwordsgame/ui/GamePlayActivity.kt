b60e71e105425c5c866c489be83cd8369453cad5
arslan.akber <arslan.akber@targetofs.com>
2019-10-21 00:43:10 +0500

    game logic defined

--- app/src/main/java/com/akber/fallingwordsgame/ui/GamePlayActivity.kt
+++ app/src/main/java/com/akber/fallingwordsgame/ui/GamePlayActivity.kt
@@ -11,9 +11,10 @@ import com.akber.fallingwordsgame.R
 import com.akber.fallingwordsgame.model.Word
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.counter_text.view.*
+import kotlin.random.Random
 
 //opened for test cases
-open class GamePlayActivity : AppCompatActivity(),View.OnClickListener, Animator.AnimatorListener {
+open class GamePlayActivity : AppCompatActivity(), View.OnClickListener, Animator.AnimatorListener {
 
     val translationIndices: ArrayList<Int> = ArrayList()
     var currentWord: Word? = null
@@ -53,6 +54,7 @@ open class GamePlayActivity : AppCompatActivity(),View.OnClickListener, Animator
         initAnimation()
 
     }
+
     override fun onAnimationRepeat(animation: Animator?) {
     }
 
@@ -66,6 +68,90 @@ open class GamePlayActivity : AppCompatActivity(),View.OnClickListener, Animator
 
     }
 
+    //get the word to place in center of screen
+    fun getNextWord(): Word {
+        translationIndices.clear()
+        return getRandomUniqueWord(false)
+    }
+
+    //check if the floating translation is correct?
+    fun isCorrectVisible(): Boolean {
+        return if (toLearnEnglish) {
+            currentWord?.word2.equals(currentTranslationOption, ignoreCase = true)
+        } else {
+            currentWord?.word1.equals(currentTranslationOption, ignoreCase = true)
+        }
+    }
+
+    //initialize the words to begin
+    fun initializeWords(words: ArrayList<Word>) {
+        wordsList?.clear()
+        wordsList?.addAll(words)
+    }
+
+    //update timer text
+    private fun updateCounterText(counter: String?) {
+        counter?.let {
+            timer_value.text = it
+        }
+    }
+
+    //get the next unique word for game, randomly picked from data set
+    //logic limited right now
+    private fun getRandomUniqueWord(isForTranslation: Boolean): Word {
+        val random = Random(System.currentTimeMillis())
+        var index: Int
+        do {
+            index = random.nextInt(0, 15)
+        } while (translationIndices.contains(index))
+
+        if (isForTranslation) {
+            translationIndices.add(index)
+        }
+        return wordsList!![index]
+    }
+
+
+    //get the next option for choosing
+    fun getNextTranslation(): Word {
+        return getRandomUniqueWord(true)
+    }
+
+    //updating count
+    private fun updateCorrectCount() {
+        correctCount += 1
+        success_counter_layout.counter_value.text = correctCount.toString()
+        checkGameLife()
+    }
+
+    //updating count
+    private fun updateWrongCount() {
+        wrongCount += 1
+        failure_counter_layout.counter_value.text = wrongCount.toString()
+        checkGameLife()
+    }
+
+    //updating count
+    private fun updateUnAnsweredCount() {
+        unAnsweredCount += 1
+        unAnswered_counter_layout.counter_value.text = unAnsweredCount.toString()
+        checkGameLife()
+    }
+
+    //checking attempts to end game
+    private fun checkGameLife() {
+        if (unAnsweredCount + correctCount + wrongCount == GAME_LIFE) {
+            gameOver()
+        }
+    }
+
+    private fun gameOver() {
+        startResultActivity()
+        finish()
+    }
+    private fun startResultActivity() {
+
+    }
 
     //setting counter titles
     private fun initViewsState() {
@@ -74,6 +160,7 @@ open class GamePlayActivity : AppCompatActivity(),View.OnClickListener, Animator
         unAnswered_counter_layout.counter_text.text = getString(R.string.missedTitle)
 
     }
+
     //initialize floating animation first time
     private fun initAnimation() {
 
@@ -87,6 +174,7 @@ open class GamePlayActivity : AppCompatActivity(),View.OnClickListener, Animator
         objectAnimator?.duration = ANSWER_TIME_DURATION
         objectAnimator?.addListener(this@GamePlayActivity)
     }
+
     companion object {
         const val ANSWER_TIME_DURATION: Long = 10000 //seconds for floating translation
         const val GAME_LIFE: Int = 25 // max attempts, includes missing as well
