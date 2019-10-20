2015b0378b2159731ae8fefc70d6a6c599cc7387
arslan.akber <arslan.akber@targetofs.com>
2019-10-20 23:43:41 +0500

    added strings, launcher activity and mnfst chngs

---
+++ app/src/main/java/com/akber/fallingwordsgame/ui/HomeActivity.kt
@@ -0,0 +1,20 @@
+package com.akber.fallingwordsgame.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+
+    }
+
+    private fun startGame(toLearnSpanish: Boolean) {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
